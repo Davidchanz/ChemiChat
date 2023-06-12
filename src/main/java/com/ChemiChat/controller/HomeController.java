b2aa39fc059be021651898598ed8511160f5793e
Davidchanz <tovliskatsi@gmail.com>
2023-06-12 15:12:32 +0400

    RabbitMQ template controller

--- src/main/java/com/ChemiChat/controller/HomeController.java
+++ src/main/java/com/ChemiChat/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.ChemiChat.controller;
 
+import com.ChemiChat.ChemiChatApplication;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -8,9 +13,17 @@ import java.security.Principal;
 @RestController
 public class HomeController {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @GetMapping("/")
     public String home(Principal principal){
         return "Hello, " + principal.getName();
+    }
+
+    @PostMapping("/send")
+    public void send(@RequestParam(value = "mess") String mess){
+        rabbitTemplate.convertAndSend(ChemiChatApplication.topicExchangeName, "foo.bar.baz", mess);
     }
 
 }
