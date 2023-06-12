f980980e64f2f3d20ffc0c89a7ce3cac41e9627d
Davidchanz <tovliskatsi@gmail.com>
2023-06-12 13:16:38 +0400

    Init commit

---
+++ src/main/java/com/ChemiChat/ChemiChatApplication.java
@@ -0,0 +1,11 @@
+package com.ChemiChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemiChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChemiChatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ChemiChat/ChemiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChemiChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
